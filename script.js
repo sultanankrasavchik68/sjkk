d879691967a77145084a8498451795fc02d9a5ff
sultanankrasavchik68 <sultananvarov34@gmail.com>
2025-03-19 21:03:56 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let  gee = prompt('isminigizi kiriting') 
+
+let gtt = prompt('Salom султан siz isminingizni hariflani katta yoki kichik qilasizmi\n 1)Katta \n 2) kchik')
+let textUp = 'султан';
+console.log(textUp.toUpperCase()); 
+
+let textLow = 'СУЛТАН';
+console.log(textLow.toLowerCase());
+alert(' 1) CУЛТАН') 
+alert('султан ismningizda nechta harf ekanligini bilishni hohlaysimi')
+alert('isminingiz 6 ta harftan iborat ')
